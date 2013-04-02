df666cacff42c0c09b17312ae4090893ff80d55c
sakares <sakares@sakares-VirtualBox.(none)>
2013-04-02 14:11:20 +0700

    @workhour=2 ,Initialize the project - This is a small prototype for our project - The concept is we have 1. start-point 2. Logic Unit 3. Report Unit - We will add feature (or Ability) to the software by difficult. Start with easiest and improve by adding harder feature.   Hence, we can see our progress from the software have more ability. - [Problem] :   I have create the main file and seperate .hpp file and feature .cpp file   for easier to separate work in the future. But I don't know why I can't compile it   if you understand what I did, please help me fix it

---
+++ project/commit.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class commits{
+	private:
+		int result;
+
+	public:
+	        commits(){
+		    result = 0;
+		}
+
+		void numberOfCommitByAuthor();	
+};
