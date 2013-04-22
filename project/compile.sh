bd66b7984bd1e1f0f83160c3d7431088b155ab8f
yoda1490 <y0da@live.fr>
2013-04-22 09:59:40 +0700

    @iterationID=5 @phase=c @workingHour=0.3 @worker=Amaury,Nuttapon @startdate=22/4/2013 @defect_inject=c @defect_type=0 @comment=Testing the templater for the slides

--- project/compile.sh
+++ project/compile.sh
@@ -1,2 +1,2 @@
-g++ readlog/read_log.cpp extractor/extractor.cpp statistic2/Team_report.cpp result_main.cpp -o finish
+g++ -I /opt/local/include readlog/read_log.cpp extractor/extractor.cpp statistic2/Team_report.cpp result_main.cpp -o finish
 
